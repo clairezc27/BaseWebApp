bd03b774856ec315f8bf10d40d0fc47d77eabd01
Claire Chen <clairezc27@gmail.com>
2019-02-16 15:44:24 -0500

    quiz results

--- project/js/main.js
+++ project/js/main.js
@@ -10,65 +10,96 @@ function myFunction() {
 }
 
 function calculation() {
+	alert("scroll down to see your result")
 	if(document.getElementById('date_Walk').checked) {
-  		console.log("date walk is checked");
   		if(document.getElementById('dress_Red').checked) {
   			if(document.getElementById('whisper').checked) {
   				if(document.getElementById('accent').checked) {
-  					
+  					$("#courtney").append(` <h3> Courtney</h3>`);
+  					$("#courtney").append(` <p> She's from atlanta woo but annoying as shit glad she got sent home </p>`);
+  					$("#courtney").append(`<img src="https://cdn1.edgedatg.com/aws/v2/abc/TheBachelor/person/2279911/610e0ae9fd1bb99e06659a0c5c347627/1600x640-Q90_610e0ae9fd1bb99e06659a0c5c347627.jpg" height = "320" width = "800"/>`);
   				} else if(document.getElementById('sloth').checked) {
-
+  					$("#nicole").append(` <h3> Nicole</h3>`);
+  					$("#nicole").append(` <p> rip marilyn and talmages favorite character she cries even more than I do </p>`);
+  					$("#nicole").append(`<img src="https://cdn1.edgedatg.com/aws/v2/abc/TheBachelor/person/2279892/7f86bfababdd25ce1d39932e09027fda/1600x640-Q90_7f86bfababdd25ce1d39932e09027fda.jpg" height = "320" width = "800"/>`);
   				}
   			} else if(document.getElementById('story').checked) {
   				if(document.getElementById('accent').checked) {
-
+  					$("#heather").append(` <h3>Heather</h3>`);
+  					$("#heather").append(` <p> never been kissed :/ ...jack is that you? </p>`);
+  					$("#heather").append(`<img src="https://cdn1.edgedatg.com/aws/v2/abc/TheBachelor/person/2279902/450af579c6534f17a9f474d1e0f6eb18/1600x640-Q90_450af579c6534f17a9f474d1e0f6eb18.jpg" height = "320" width = "800"/>`);
   				} else if(document.getElementById('sloth').checked) {
-
+  					$("#tracy").append(` <h3>Tracy</h3>`);
+  					$("#tracy").append(` <p> AYEEE you got aardvark girl :/ no one really likes her sorry </p>`);
+  					$("#tracy").append(`<img src="https://cdn1.edgedatg.com/aws/v2/abc/TheBachelor/person/2279876/facab09010a008c6d04ddea0ac2d67aa/1600x640-Q90_facab09010a008c6d04ddea0ac2d67aa.jpg" height = "320" width = "800"/>`);
   				}
   			}
   		} else if(document.getElementById('dress_Glitter').checked) {
-  			if(document.getElementById('dress_Red').checked) {
+  			if(document.getElementById('whisper').checked) {
   				if(document.getElementById('accent').checked) {
-
+  					$("#onyeka").append(` <h3>Onyeka</h3>`);
+  					$("#onyeka").append(` <p> we all liked oneyka until her dumb drama with nicloe. bye felicia. </p>`);
+  					$("#onyeka").append(`<img src="https://cdn1.edgedatg.com/aws/v2/abc/TheBachelor/person/2279887/11b2a11d4c0471c3b3b439e9bbc729d8/1600x640-Q90_11b2a11d4c0471c3b3b439e9bbc729d8.jpg" height = "320" width = "800"/>`);
   				} else if(document.getElementById('sloth').checked) {
-
+  					$("#nicole").append(` <h3> Nicole</h3>`);
+  					$("#nicole").append(` <p> rip marilyn and talmages favorite character she cries even more than I do </p>`);
+  					$("#nicole").append(`<img src="https://cdn1.edgedatg.com/aws/v2/abc/TheBachelor/person/2279892/7f86bfababdd25ce1d39932e09027fda/1600x640-Q90_7f86bfababdd25ce1d39932e09027fda.jpg" height = "320" width = "800"/>`);
   				}
-  			} else if(document.getElementById('dress_Glitter').checked) {
+  			} else if(document.getElementById('story').checked) {
   				if(document.getElementById('accent').checked) {
-
+  					$("#caelynn").append(` <h3> Caelynn</h3>`);
+  					$("#caelynn").append(` <p> Caelynn is so sweet and gorgeous!! But tbh she only got this far bc of her tragic sob story </p>`);
+  					$("#caelynn").append(`<img src="https://cdn1.edgedatg.com/aws/v2/abc/TheBachelor/person/2279916/6db8529ca2a1bb29ae424743060d0cbc/1600x640-Q90_6db8529ca2a1bb29ae424743060d0cbc.jpg" height = "320" width = "800"/>`);
   				} else if(document.getElementById('sloth').checked) {
-
+  					$("#cassie").append(` <h3> Cassie</h3>`);
+  					$("#cassie").append(` <p> yayyy everyone likes cassie shes so pretty and amazing GO YOU </p>`);
+  					$("#cassie").append(`<img src="https://cdn1.edgedatg.com/aws/v2/abc/TheBachelor/person/2279914/d882a573ac26ff1705ca8257903b49ff/1600x640-Q90_d882a573ac26ff1705ca8257903b49ff.jpg" height = "320" width = "800"/>`);
   				}
   			}
   		}
 	}else if(document.getElementById('date_Movie').checked) {
-		console.log("date movie is checked");
 		if(document.getElementById('dress_Red').checked) {
   			if(document.getElementById('whisper').checked) {
-  				if(document.getElementById('whisper').checked) {
-
-  				} else if(document.getElementById('story').checked) {
-
+  				if(document.getElementById('accent').checked) {
+  					$("#courtney").append(` <h3> Courtney</h3>`);
+  					$("#courtney").append(` <p> She's from atlanta woo but annoying as shit glad she got sent home </p>`);
+  					$("#courtney").append(`<img src="https://cdn1.edgedatg.com/aws/v2/abc/TheBachelor/person/2279911/610e0ae9fd1bb99e06659a0c5c347627/1600x640-Q90_610e0ae9fd1bb99e06659a0c5c347627.jpg" height = "320" width = "800"/>`);
+  				} else if(document.getElementById('sloth').checked) {
+  					$("#demi").append(` <h3>Demi</h3>`);
+  					$("#demi").append(` <p> Demis a bitch. But also hilarious. You can take this as a compliment or an insult. </p>`);
+  					$("#demi").append(`<img src="https://cdn1.edgedatg.com/aws/v2/abc/TheBachelor/person/2279910/1c1869345740daa3b04c8d1ceea7c852/1600x640-Q90_1c1869345740daa3b04c8d1ceea7c852.jpg" height = "320" width = "800"/>`);
   				}
   			} else if(document.getElementById('story').checked) {
-  				if(document.getElementById('whisper').checked) {
-
-  				} else if(document.getElementById('story').checked) {
-
+  				if(document.getElementById('accent').checked) {
+  					$("#heather").append(` <h3>Heather</h3>`);
+  					$("#heather").append(` <p> never been kissed :/ ...jack is that you? </p>`);
+  					$("#heather").append(`<img src="https://cdn1.edgedatg.com/aws/v2/abc/TheBachelor/person/2279902/450af579c6534f17a9f474d1e0f6eb18/1600x640-Q90_450af579c6534f17a9f474d1e0f6eb18.jpg" height = "320" width = "800"/>`);
+  				} else if(document.getElementById('sloth').checked) {
+  					$("#tracy").append(` <h3>Tracy</h3>`);
+  					$("#tracy").append(` <p> AYEEE you got aardvark girl :/ no one really likes her sorry </p>`);
+  					$("#tracy").append(`<img src="https://cdn1.edgedatg.com/aws/v2/abc/TheBachelor/person/2279876/facab09010a008c6d04ddea0ac2d67aa/1600x640-Q90_facab09010a008c6d04ddea0ac2d67aa.jpg" height = "320" width = "800"/>`);
   				}
   			}
   		} else if(document.getElementById('dress_Glitter').checked) {
   			if(document.getElementById('whisper').checked) {
-  				if(document.getElementById('whisper').checked) {
-
-  				} else if(document.getElementById('story').checked) {
-
+  				if(document.getElementById('accent').checked) {
+  					$("#onyeka").append(` <h3>Onyeka</h3>`);
+  					$("#onyeka").append(` <p> we all liked oneyka until her dumb drama with nicloe. bye felicia. </p>`);
+  					$("#onyeka").append(`<img src="https://cdn1.edgedatg.com/aws/v2/abc/TheBachelor/person/2279887/11b2a11d4c0471c3b3b439e9bbc729d8/1600x640-Q90_11b2a11d4c0471c3b3b439e9bbc729d8.jpg" height = "320" width = "800"/>`);
+  				} else if(document.getElementById('sloth').checked) {
+  					$("#demi").append(` <h3>Demi</h3>`);
+  					$("#demi").append(` <p> Demis a bitch. But also hilarious. You can take this as a compliment or an insult. </p>`);
+  					$("#demi").append(`<img src="https://cdn1.edgedatg.com/aws/v2/abc/TheBachelor/person/2279910/1c1869345740daa3b04c8d1ceea7c852/1600x640-Q90_1c1869345740daa3b04c8d1ceea7c852.jpg" height = "320" width = "800"/>`);
   				}
   			} else if(document.getElementById('story').checked) {
-  				if(document.getElementById('whisper').checked) {
-
-  				} else if(document.getElementById('story').checked) {
-
+  				if(document.getElementById('accent').checked) {
+  					$("#caelynn").append(` <h3> Caelynn</h3>`);
+  					$("#caelynn").append(` <p> Caelynn is so sweet and gorgeous!! But tbh she only got this far bc of her tragic sob story </p>`);
+  					$("#caelynn").append(`<img src="https://cdn1.edgedatg.com/aws/v2/abc/TheBachelor/person/2279916/6db8529ca2a1bb29ae424743060d0cbc/1600x640-Q90_6db8529ca2a1bb29ae424743060d0cbc.jpg" height = "320" width = "800"/>`);
+  				} else if(document.getElementById('sloth').checked) {
+  					$("#cassie").append(` <h3> Cassie</h3>`);
+  					$("#cassie").append(` <p> yayyy everyone likes cassie shes so pretty and amazing GO YOU </p>`);
+  					$("#cassie").append(`<img src="https://cdn1.edgedatg.com/aws/v2/abc/TheBachelor/person/2279914/d882a573ac26ff1705ca8257903b49ff/1600x640-Q90_d882a573ac26ff1705ca8257903b49ff.jpg" height = "320" width = "800"/>`);
   				}
   			}
   		}
